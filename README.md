98106d4775cae0d5e0db5733c8cc55ddcd379de4
Markus Poeschl <Poeschl@users.noreply.github.com>
2019-09-21 15:24:23 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# remote picoTTS Home Assistant Component
+
+This is a new component for [Home-Assitant](https://www.home-assistant.io/) which integrates picoTTS as a remote server. The server is running inside my [Hass.io picoTTS Addon](https://github.com/Poeschl/Hassio-Addons/tree/master/picoTTS).
+
+# Installation
+
+To use it, copy the `picotts_remote` folder inside your `config/custom_components` folder on your home assistant installation first.
+
+Afterwards add following config to your yaml:
+
+```yaml
+tts:
+  - platform: picotts
+    hostname: <host>
+    port: <port>
+
+```
+
+# Note
+
+I intend to integrate this one in the official home assistant components, but got only limited time right now. If you want to help out, go on.
---
+++ picotts_remote/tts.py
@@ -0,0 +1,88 @@
+"""Support for the Pico TTS speech service."""
+import logging
+
+import asyncio
+import aiohttp
+import re
+import async_timeout
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.tts import CONF_LANG, PLATFORM_SCHEMA, Provider
+from homeassistant.const import CONF_HOST, CONF_PORT
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from urllib.parse import quote
+
+
+_LOGGER = logging.getLogger(__name__)
+
+SUPPORT_LANGUAGES = ["en-US", "en-GB", "de-DE", "es-ES", "fr-FR", "it-IT"]
+
+DEFAULT_LANG = "en-US"
+DEFAULT_HOST = "localhost"
+DEFAULT_PORT = 59126
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Optional(CONF_LANG, default=DEFAULT_LANG): vol.In(SUPPORT_LANGUAGES),
+        vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string,
+        vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port
+    }
+)
+
+
+def get_engine(hass, config):
+    """Set up Pico speech component."""
+    return PicoProvider(hass, config[CONF_LANG], config[CONF_HOST], config[CONF_PORT])
+
+
+class PicoProvider(Provider):
+    """The Pico TTS API provider."""
+
+    def __init__(self, hass, lang, host, port):
+        """Initialize Pico TTS provider."""
+        self._hass = hass
+        self._lang = lang
+        self._host = host
+        self._port = port
+        self.name = "PicoTTS (Remote)"
+
+    @property
+    def default_language(self):
+        """Return the default language."""
+        return self._lang
+
+    @property
+    def supported_languages(self):
+        """Return list of supported languages."""
+        return SUPPORT_LANGUAGES
+
+    async def async_get_tts_audio(self, message, language, options=None):
+        """Load TTS using a remote pico2wave server."""
+        websession = async_get_clientsession(self._hass)
+
+        try:
+            with async_timeout.timeout(5):
+                url = "http://{}:{}/speak?".format(self._host, self._port)
+                encoded_message = quote(message)
+                url_param = {
+                    "lang": language,
+                    "text": encoded_message,
+                }
+
+                request = await websession.get(url, params=url_param)
+
+                if request.status != 200:
+                    _LOGGER.error(
+                        "Error %d on load url %s", request.status, request.url
+                    )
+                    return (None, None)
+                data = await request.read()
+
+        except (asyncio.TimeoutError, aiohttp.ClientError):
+            _LOGGER.error("Timeout for PicoTTS API")
+            return (None, None)
+
+        if data:
+            return ("wav", data)
+        return (None, None)
